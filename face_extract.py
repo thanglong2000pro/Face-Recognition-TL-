10318f95611e0e8a94435c8dc4ec23ab67c73ca7
thanglong2000pro <laphungtien2000@gmail.com>
2020-12-05 12:37:40 +0700

    train model and image recognition

--- face_extract.py
+++ face_extract.py
@@ -1,8 +1,10 @@
 # CACH DUNG LENH
-# python face_extract.py --input_dir videos/ --output_dir dataset/
+# python face_extract.py --input_dir videos/ --embeddings output/embeddings.pickle --embedding-model openface_nn4.small2.v1.t7
 
 import numpy as np
 import argparse
+import imutils
+import pickle
 import cv2
 import os
 
@@ -10,12 +12,14 @@ import os
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input_dir", type=str, required=True,
                 help="path to input video")
-ap.add_argument("-o", "--output_dir", type=str, required=True,
-                help="path to output directory of cropped faces")
 ap.add_argument("-d", "--detector", type=str, default='face_detector',
                 help="path to OpenCV's deep learning face detector")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
                 help="minimum probability to filter weak detections")
+ap.add_argument("-m", "--embedding-model", required=True,
+	help="path to OpenCV's deep learning face embedding model")
+ap.add_argument("-e", "--embeddings", required=True,
+	help="path to output serialized db of facial embeddings")
 ap.add_argument("-s", "--skip", type=int, default=1,
                 help="# of frames to skip before applying face detection")
 args = vars(ap.parse_args())
@@ -25,11 +29,25 @@ print("[INFO] loading face detector...")
 protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
 modelPath = os.path.sep.join([args["detector"],
                               "res10_300x300_ssd_iter_140000.caffemodel"])
-net = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+# load our serialized face embedding model from disk
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
+
+# initialize our lists of extracted facial embeddings and
+# corresponding people names
+knownEmbeddings = []
+knownNames = []
+# initialize the total number of faces processed
+total = 0
 
 # Doc file video input
 for filename in os.listdir(args["input_dir"]):
     input = args["input_dir"] + filename
+    # lay label
+    name = filename.split('.')[0]
+    # bat video, tach frame va xu li tung frame
     vs = cv2.VideoCapture(input)
     read = 0
     saved = 0
@@ -52,12 +70,12 @@ for filename in os.listdir(args["input_dir"]):
                                      (300, 300), (104.0, 177.0, 123.0))
 
         # Phat hien cac khuon mat trong frame
-        net.setInput(blob)
-        detections = net.forward()
+        detector.setInput(blob)
+        detections = detector.forward()
 
         # Neu tim thay it nhat 1 khuon mat
         if len(detections) > 0:
-            # Tim khuon  mat to nhat trong anh
+            # Tim khuon mat to nhat trong anh
             i = np.argmax(detections[0, 0, :, 2])
             confidence = detections[0, 0, i, 2]
 
@@ -66,16 +84,34 @@ for filename in os.listdir(args["input_dir"]):
                 # Tach khuon mat va ghi ra file
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
-                face = frame[startY:endY, startX:endX]
 
-                # write the frame to disk
-                output = args["output_dir"] + filename
-                output = output.split('.') #tach duoi mp4
-                p = os.path.sep.join([output[0],
-                                      input.split('/')[1] + "{}.png".format(saved)])
-                cv2.imwrite(p, face)
-                saved += 1
-                print("[INFO] saved {} to disk".format(p))
+                # extract the face ROI and grab the ROI dimensions
+                face = frame[startY:endY, startX:endX]
+                (fH, fW) = face.shape[:2]
+
+                # ensure the face width and height are sufficiently large
+                if fW < 20 or fH < 20:
+                    continue
+
+                # construct a blob for the face ROI, then pass the blob
+                # through our face embedding model to obtain the 128-d
+                # quantification of the face
+                faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+                                                     (96, 96), (0, 0, 0), swapRB=True, crop=False)
+                embedder.setInput(faceBlob)
+                vec = embedder.forward()
+                # add the name of the person + corresponding face
+                # embedding to their respective lists
+                knownNames.append(name)
+                knownEmbeddings.append(vec.flatten())
+                total += 1
 
     vs.release()
     cv2.destroyAllWindows()
+
+# dump the facial embeddings + names to disk
+print("[INFO] serializing {} encodings...".format(total))
+data = {"embeddings": knownEmbeddings, "names": knownNames}
+f = open(args["embeddings"], "wb")
+f.write(pickle.dumps(data))
+f.close()
