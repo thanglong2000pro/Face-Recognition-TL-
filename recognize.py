b9b66937ef5481d9b59ccb7d17062fd49335ccae
thanglong2000pro <laphungtien2000@gmail.com>
2020-12-05 16:40:29 +0700

    create recognize video and evaluate

--- recognize.py
+++ recognize.py
@@ -90,9 +90,9 @@ for i in range(0, detections.shape[2]):
     text = "{}: {:.2f}%".format(name, proba * 100)
     y = startY - 10 if startY - 10 > 10 else startY + 10
     cv2.rectangle(image, (startX, startY), (endX, endY),
-                  (0, 0, 255), 2)
+                  (0, 255, 0), 2)
     cv2.putText(image, text, (startX, y),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
 
 # show the output image
 cv2.imshow("Image", image)
--- train_model.py
+++ train_model.py
@@ -2,7 +2,11 @@
 # python train_model.py --embeddings output/embeddings.pickle --recognizer output/recognizer.pickle --le output/le.pickle
 
 # import the necessary packages
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 import argparse
 import pickle
@@ -26,11 +30,24 @@ print("[INFO] encoding labels...")
 le = LabelEncoder()
 labels = le.fit_transform(data["names"])
 
+# chia tập data thành 80% tập train và 20% tập test
+(trainX, testX, trainY, testY) = train_test_split(data["embeddings"], labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
 # train the model used to accept the 128-d embeddings of the face and
 # then produce the actual face recognition
 print("[INFO] training model...")
 recognizer = SVC(C=1.0, kernel="linear", probability=True)
-recognizer.fit(data["embeddings"], labels)
+recognizer.fit(trainX, trainY)
+
+# predict
+y_train_predict = recognizer.predict(trainX)
+y_test_predict = recognizer.predict(testX)
+# score
+score_train = accuracy_score(trainY, y_train_predict)
+score_test = accuracy_score(testY, y_test_predict)
+# summarize
+print('Accuracy: train=%.3f, test=%.3f' % (score_train*100, score_test*100))
 
 # write the actual face recognition model to disk
 f = open(args["recognizer"], "wb")
