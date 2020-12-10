93f992088a0ce4210e3ddd5f2ac7ee07c5f070ec
thanglong2000pro <laphungtien2000@gmail.com>
2020-12-10 23:01:58 +0700

    update extract frame

--- extract_frame.py
+++ extract_frame.py
@@ -1,10 +1,7 @@
 # CACH DUNG LENH
 # python extract_frame.py --input_dir videos/ --output_dir dataset/
 
-import numpy as np
 import argparse
-import imutils
-import pickle
 import cv2
 import os
 
@@ -16,18 +13,25 @@ ap.add_argument("-o", "--output_dir", type=str, required=True,
                 help="path to output directory of cropped faces")
 ap.add_argument("-s", "--skip", type=int, default=1,
                 help="# of frames to skip before applying face detection")
+ap.add_argument("-n", "--num-to-train", type=int, default=80,
+                help="# num of frames to train")
 args = vars(ap.parse_args())
 
 # Doc file video input
 for filename in os.listdir(args["input_dir"]):
     input = args["input_dir"] + filename
     vs = cv2.VideoCapture(input)
+    num_frame = vs.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    no_frame = 0
     read = 0
     saved = 0
+    frameRate = int(num_frame / args['num_to_train'])
 
     # Lap qua cac frame cua video
     while True:
 
+        vs.set(cv2.CAP_PROP_POS_FRAMES, no_frame)
         (grabbed, frame) = vs.read()
         # Neu khong doc duoc frame thi thoat
         if not grabbed:
@@ -48,6 +52,7 @@ for filename in os.listdir(args["input_dir"]):
                                   input.split('/')[1] + "{}.png".format(saved)])
         cv2.imwrite(p, frame)
         saved += 1
+        no_frame += frameRate
         print("[INFO] saved {} to disk".format(p))
 
 vs.release()
